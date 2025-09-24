f2a361cd547bf7deb293ac552f2b0b3525a420ad
amitverma8835 <amitkv93047@gmail.com>
2025-09-24 11:00:58 +0530

    vite.config.js file updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      'guest-taazadandiya-admin.onrender.com'
+    ]
+  }
 })
